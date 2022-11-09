9dae483354adb41607bcc48c85a155b71a831e7c
Jeremiah224468 <111754998+Jeremiah224468@users.noreply.github.com>
2022-11-09 08:45:10 -0600

    uiluil

--- game/colluseum.js
+++ game/colluseum.js
@@ -8,3 +8,17 @@ colluseum.load = function(){
     this.fighters[0] = fighter.new("Warrior", 10, 3, 6);
     this.fighters[1] = fighter.new("Monk", 3, 9, 1);
 }
+
+     colluseum.go = function(){
+        let fighter1 = this.fighter[0];
+        let fighter2 = this.fighters[1];
+        while(fighter1.hp > 0 && fighter2.hp > 0){
+            if(this.FirstPlayersTurn){
+                fighter1.hp = fighter1.hp - 2;
+            }else{
+
+            }
+            fighter2.hp = fighter2.hp - 2;
+        }
+        this.firstPlayersTurn = !this.firstPlayersTurn
+     }
